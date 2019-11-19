3a8714ff2ab18d65051302c81166cf7f013446d3
LongDT <doan.the.long@moneyforward.co.jp>
2019-11-19 18:10:12 +0900

    added sign up page and save user to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
-            uniqueness: true
+            uniqueness: {case_sensitive: true}
   validates :password, presence: true, length: { minimum: 6 }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'landing_pages#home'
   get '/:locale', to: 'landing_pages#home', constraints: { locale: /jp/ }, as: 'home'
   get '/sign_up', to: 'users#new'
+  resources :users
 end
