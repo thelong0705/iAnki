0eb843ce4e081cea9ad6feb9ce79cc022e5f0d78
LongDT <doan.the.long@moneyforward.co.jp>
2019-11-19 15:48:28 +0900

    created link to sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'landing_pages#home'
   get '/:locale', to: 'landing_pages#home', constraints: { locale: /jp/ }, as: 'home'
+  get '/sign_up', to: 'users#new'
 end
