0be930dbca35f45bf28cd428e3d09cc156a2a4d2
LongDT <doan.the.long@moneyforward.co.jp>
2019-11-20 17:28:27 +0900

    added sign up by modal function

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: {case_sensitive: true}
-  validates :password, presence: true, length: { minimum: 6 }
 
+  validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'landing_pages#home'
   get '/:locale', to: 'landing_pages#home', constraints: { locale: /jp/ }, as: 'home'
-  get '/sign_up', to: 'users#new'
-  resources :users
+  resources :users, except: :new
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 end
