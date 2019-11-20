3f10018c2bac70d9fbffd40fb385e11d22d93374
LongDT <doan.the.long@moneyforward.co.jp>
2019-11-20 15:47:22 +0900

    added login by modal function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:session][:email])
+    @user = User.find_by(email: params[:session][:email])
 
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
-    else
-      render 'new'
-
+    respond_to do |format|
+      if @user && @user.authenticate(params[:session][:password])
+        log_in @user
+        format.html { redirect_to @user }
+      else
+        format.js
+      end
     end
   end
 
