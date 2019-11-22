3c7e8861e8017c1b5a55fd7ef16124ac6c18f220
LongDT <thelong0705@gmail.com>
2019-11-22 14:27:01 +0900

    added sending mail using real email

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,7 +2,7 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.active
+      user.activate
       log_in user
       redirect_to home_url
     else
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,8 +60,18 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :test
   host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.default_url_options = { :host => host, protocol: 'http' }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => "587",
+      :user_name            => ENV['EMAIL_USERNAME'],
+      :password             => ENV['EMAIL_PASSWORD'],
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+  }
 end
