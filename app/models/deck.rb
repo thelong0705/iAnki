503c310f9cfa4f99ebf9d941cc384819bf742084
LongDT <thelong0705@gmail.com>
2019-11-26 11:25:15 +0900

    added reject blank card

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,5 +1,6 @@
 class Deck < ApplicationRecord
   belongs_to :user
   has_many :cards
-  accepts_nested_attributes_for :cards
+  accepts_nested_attributes_for :cards,
+                                reject_if: lambda {|attributes| attributes['question'].blank?}
 end
