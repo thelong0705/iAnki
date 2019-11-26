620a4b92731e90669337ade2c033acd1412f528e
LongDT <thelong0705@gmail.com>
2019-11-26 18:15:52 +0900

    added update and delete card

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -17,9 +17,20 @@ class DecksController < ApplicationController
     @deck = Deck.find(params[:id])
   end
 
+  def edit
+    @deck = Deck.find(params[:id])
+  end
+
+  def update
+    @deck = Deck.find(params[:id])
+    @deck.update(deck_params)
+    redirect_to @deck
+  end
+
+
   private
 
   def deck_params
-    params.require(:deck).permit(:name, cards_attributes: [:question, :answer])
+    params.require(:deck).permit(:id, :name, cards_attributes: [:id, :question, :answer, :_destroy])
   end
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,5 +2,6 @@ class Deck < ApplicationRecord
   belongs_to :user
   has_many :cards
   accepts_nested_attributes_for :cards,
-                                reject_if: lambda {|attributes| attributes['question'].blank?}
+                                reject_if: :all_blank,
+                                allow_destroy: true
 end
