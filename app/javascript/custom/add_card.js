5ff289edb4a5639aaa446ab8ffef0903c4de8815
LongDT <thelong0705@gmail.com>
2019-11-27 10:22:03 +0900

    generate unique id by timestamps

--- app/javascript/custom/add_card.js
+++ app/javascript/custom/add_card.js
@@ -1,17 +1,23 @@
 $(document).ready(function () {
     let addNewRow = () => {
-        let unique_id = parseInt($(".card-form-row:last").attr('id')) + 1;
+        let unique_id = + new Date();
         $(".card-form-row:last").parent().after(
             `
             <div class="form-group">
-              <div class="row card-form-row" id="${unique_id}">
-                <div class="col">
-                  <input class="form-control" placeholder="Question" type="text" name="deck[cards_attributes][${unique_id}][question]">
+                <div class="row card-form-row" id="${unique_id}">
+                    <div class="col-6">
+                        <input class="form-control" placeholder="Question" type="text" name="deck[cards_attributes][${unique_id}][question]">
+                    </div>
+                    <div class="col-5">
+                        <input class="form-control" placeholder="Answer" type="text" name="deck[cards_attributes][${unique_id}][answer]">
+                     </div>
+                    <div class="col-1 float-right">
+                        <input type="hidden" value="false" name="deck[cards_attributes][${unique_id}][_destroy]">
+                        <button class="btn btn-danger btn-sm deleteRow">
+                           <i class="fas fa-trash"></i>
+                        </button>
+                    </div>
                 </div>
-                <div class="col">
-                  <input class="form-control" placeholder="Answer" type="text" name="deck[cards_attributes][${unique_id}][answer]">
-                </div>
-              </div>
             </div>
             `
         );
