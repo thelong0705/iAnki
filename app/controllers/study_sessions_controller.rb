535522519c09888ad941a89b00c884dc757f3a95
LongDT <thelong0705@gmail.com>
2019-11-28 11:15:12 +0900

    added learn again function

--- app/controllers/study_sessions_controller.rb
+++ app/controllers/study_sessions_controller.rb
@@ -1,21 +1,29 @@
 class StudySessionsController < ApplicationController
   before_action :set_card, only: :show
-  after_action :set_card, only: :update
 
   def show
   end
 
 
   def update
-    return if params[:commit] == "Study again"
-    last_card = Card.find(params[:card_id])
-    last_card.update(is_remembered: true)
+    if params[:commit] == "Learn again"
+      deck = Deck.find(params[:id])
+      deck.cards.update_all is_remembered: false
+      redirect_to study_session_path(deck)
+    end
+
+    if params[:commit] == "Got it"
+      Card.find(params[:card_id]).update(is_remembered: true)
+    end
+
+    set_card
   end
 
   private
-    def set_card
-      @deck = Deck.find(params[:id])
-      random_id = @deck.cards.not_remembered.pluck(:id).sample
-      @card = Card.find_by(id: random_id)
-    end
+
+  def set_card
+    @deck = Deck.find(params[:id])
+    random_id = @deck.cards.not_remembered.pluck(:id).sample
+    @card = Card.find_by(id: random_id)
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("channels")
 require("jquery")
 require("custom/add_card")
 require("custom/delete_card")
-
+require("custom/flip_card")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
