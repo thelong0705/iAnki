f3e3bab29102d7ac19581ab1d0eb6669e1ea009d
LongDT <thelong0705@gmail.com>
2019-12-02 10:45:54 +0900

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  User.create!(
+      {
+          name: Faker::Name.name,
+          email: Faker::Internet.email,
+          password: "password",
+          activated: true
+      }
+  )
+end
+
+100.times do
+  card_attributes = (1..10).reduce([]) do |array|
+    array.push({ question: Faker::Lorem.word, answer: Faker::Lorem.word })
+  end
+
+  Deck.create!(
+      {
+          user: User.all.sample,
+          name: Faker::Lorem.word,
+          cards_attributes: card_attributes
+      }
+  )
+end
