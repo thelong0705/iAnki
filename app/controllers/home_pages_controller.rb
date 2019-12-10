b090cceb1b9ac798484a718d611dfb0bd4740bf9
LongDT <thelong0705@gmail.com>
2019-12-10 16:40:56 +0900

    add id for login button in home page

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,5 +1,10 @@
 class HomePagesController < ApplicationController
   def index
+    unless current_user
+      redirect_to root_url
+      return
+    end
+
     @decks = current_user.decks
   end
 end
