2ee6a442fe63aa2ec946e8c3c81f4f54b20cc6aa
LongDT <thelong0705@gmail.com>
2019-12-12 17:56:29 +0900

    i18n for mail sending

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,15 @@
 class UserMailer < ApplicationMailer
-
   def account_activation(user, activation_token)
     @user = user
     @activation_token = activation_token
-    mail to: user.email, subject: "Account activation"
+    I18n.locale = @user.locale
+    mail to: user.email, subject: I18n.t(:account_activation)
   end
 
 
   def password_reset(user)
     @user = user
+    I18n.locale = @user.locale
     mail to: user.email, subject: "Password reset"
   end
 end
